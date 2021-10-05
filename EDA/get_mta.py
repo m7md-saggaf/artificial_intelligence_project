60a00a4fd470088fdba547a44a23b7797cbcc6ab
m7md-saggaf <61165994+m7md-saggaf@users.noreply.github.com>
2021-10-05 06:03:01 +0300

    added my data analysis process

---
+++ EDA/get_mta.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:87ef0ca23fd614bbf6f3efdecaff6ee325c4d2ef6efec4aa77d9f59f48598d95
+size 2079
---
+++ EDA/mvp_checkpoint_final.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a9b8944daae2b5709ba7ef58ab86ad9a8ffadabb68daa07a9ac106f09916b9b0
+size 1529
